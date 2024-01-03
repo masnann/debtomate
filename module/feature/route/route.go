b1b75729d47c700614db1a41e535215384cf996d
Anan Widianto <anan.widi1@gmail.com>
2024-01-03 23:27:08 +0700

    feature: auth middleware

--- module/feature/route/route.go
+++ module/feature/route/route.go
@@ -3,13 +3,14 @@ package route
 import (
 	"debtomate/module/feature/auth"
 	"debtomate/module/feature/borrowers"
+	"debtomate/utils/token"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
 
-func SetupRoutes(app *fiber.App, db *gorm.DB) {
+func SetupRoutes(app *fiber.App, db *gorm.DB, jwt token.JWTInterface) {
 	auth.InitializeAuth(db)
 	auth.SetupRoutesAuth(app)
 	borrowers.InitializeBorrowers(db)
-	borrowers.SetupRoutesBorrowers(app)
+	borrowers.SetupRoutesBorrowers(app, jwt)
 }
